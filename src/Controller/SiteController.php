e3548302ffd0cd6c33afe44f45fd4699a2eff524
Michal <matej50@gmail.com>
2021-04-20 05:16:44 +0200

    Changed new and edit functions  to one new function

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -14,12 +14,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SiteController extends AbstractController
 {
+
     /**
      * @Route("/site", name="site")
      */
     public function index(Request $request): Response
-    { $user = $this->getUser()->getId();
-        $sites = $this->getDoctrine()->getRepository(Site::class)->findBy(['user'=>$user]);
+    {
+        $user = $this->getUser()->getId();
+        $sites = $this->getDoctrine()->getRepository(Site::class)->findBy(['user' => $user]);
         return $this->render('site/index.html.twig', [
             'sites' => $sites
         ]);
@@ -27,67 +29,56 @@ class SiteController extends AbstractController
 
     /**
      * @Route("/site/new", name="new_site")
-     */
-    public function newHost(Request $request): Response
-    {
-
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-
-            $site = new Site();
-            $form = $this->createForm(SiteType::class, $site);
-            $form->handleRequest($request);
-            if ($form->isSubmitted() && $form->isValid()) {
-
-                $site = $form->getData();
-                $em = $this->getDoctrine()->getManager();
-
-                $em->persist($site);
-                $em->flush();
-
-
-                return $this->redirect($this->generateUrl('site'));
-            }
-
-
-            return $this->render('site/NewPage.html.twig', [
-                'form' => $form->createView(),
-            ]);
-        }
-
-
-    /**
      * @Route("/site/edit/{id}", name="site_edit")
      *
      */
-    public function edit(Request $request, $id)
+    public function edit(Request $request, $id = null)
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-        $site = new Site();
-        $site = $this->getDoctrine()->getRepository(Site::class)->find($id);
 
-        $siteCheck = new SiteChecks();
-        $siteCheck = $this->getDoctrine()->getRepository(SiteChecks::class)->findBy(['site'=>$site],['id'=>'DESC'], 10);
+        if ($id != null) {
+            $site = $this->getDoctrine()->getRepository(Site::class)->find($id);
+            $isEdit = true;
+        } else {
+            $site = new Site();
+            $isEdit = false;
+        }
 
+        $siteCheck = $this->getDoctrine()->getRepository(SiteChecks::class)->findBy(['site' => $site], ['id' => 'DESC'], 10);
 
         $form = $this->createForm(SiteType::class, $site, [
-            'is_edit' => true,
+            'is_edit' => $isEdit,
         ]);
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
-            $em->flush();
 
+            if ($id == null) {
+                $site = $form->getData();
+                $em->persist($site);
+                $em->flush();
+
+                return $this->redirectToRoute('site');
+            }
+
+            $em->flush();
             return $this->redirectToRoute('site');
+
         }
 
-
+        if ($id == null) {
+            return $this->render('site/NewPage.html.twig', [
+                'form' => $form->createView(),
+            ]);
+        } else {
             return $this->render('site/edit.html.twig', [
                 'form' => $form->createView(),
                 'siteCheck' => $siteCheck
             ]);
         }
+    }
 
     /**
      * @Route("/site/delete/{id}", name="site_delete")
@@ -97,15 +88,13 @@ class SiteController extends AbstractController
     {
 
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-            $em = $this->getDoctrine()->getManager();
-            $entry = $em->getRepository(Site::class)->find($id);
+        $em = $this->getDoctrine()->getManager();
+        $entry = $em->getRepository(Site::class)->find($id);
 
-            $em->remove($entry);
-            $em->flush();
-            return $this->redirectToRoute('site');
-        }
-
-
+        $em->remove($entry);
+        $em->flush();
+        return $this->redirectToRoute('site');
+    }
 
 
 }
