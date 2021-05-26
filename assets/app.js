96a72442c2c9c7b433b3da016477bb5c78f69b40
Michal <matej50@gmail.com>
2021-05-26 09:20:08 +0200

    Delete unnecessary files

--- assets/app.js
+++ assets/app.js
@@ -3,7 +3,7 @@ import './bootstrap';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import JsonConfiguration from "./components/jsonConfiguration";
-import './components/index';
+
 if(document.getElementById('site_test_configuration')) {
     ReactDOM.render(<JsonConfiguration init-value={JSON.parse(document.getElementById('site_test_configuration').value)}/>, document.getElementById('type-configuration'));
 }
